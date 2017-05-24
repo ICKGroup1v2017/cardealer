250d516ad219b8b95bcafada1cb1a06008850cdf
Ahmet Murati <ahmet.murati@live.com>
2017-05-24 17:41:38 +0200

    First version a3

--- src/cardealer/models/Sale.java
+++ src/cardealer/models/Sale.java
@@ -5,15 +5,106 @@
  */
 package cardealer.models;
 
+import java.sql.Date;
+
 /**
  *
  * @author ahmet
  */
 public class Sale implements Comparable<Sale> {
-    private Long id;
+    private Long idSale;
 
+   private String firstName;
+   private String lastName;
+   private double price;
+   private Long car_id;
+   private java.sql.Date sale_date;
+   private java.sql.Date paid_date;
+
+    public Sale() {
+       this.idSale = 0l;
+        this.firstName = "";
+        this.lastName = "";
+        this.price = 0l;
+        this.car_id = 0l;
+        this.sale_date = new Date(new java.util.Date().toInstant().toEpochMilli());
+        this.paid_date = null;
+    }
+    
+    
+
+    public Sale(Long idSale, String firstName, String lastName, double price, Long car_id, Date sale_date, Date paid_date) {
+        this.idSale = idSale;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.price = price;
+        this.car_id = car_id;
+        this.sale_date = sale_date;
+        this.paid_date = paid_date;
+    }
+   
+   
+
+    public Long getId() {
+        return idSale;
+    }
+
+    public void setId(Long idSale) {
+        this.idSale = idSale;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Long getCar_id() {
+        return car_id;
+    }
+
+    public void setCar_id(Long car_id) {
+        this.car_id = car_id;
+    }
+
+    public Date getSale_date() {
+        return sale_date;
+    }
+
+    public void setSale_date(Date sale_date) {
+        this.sale_date = sale_date;
+    }
+
+    public Date getPaid_date() {
+        return paid_date;
+    }
+
+    public void setPaid_date(Date paid_date) {
+        this.paid_date = paid_date;
+    }
+   
+   
+   
     @Override
     public int compareTo(Sale o) {
-        return this.id.compareTo(o.id);
+        return this.idSale.compareTo(o.idSale);
     }
 }
